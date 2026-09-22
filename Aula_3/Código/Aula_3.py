8f25b8ada98dfbf0b5179c04ad26e3bd2d68275d
=AdalbertoEQ <=jose.martins@eq.ufcg.edu.br>
2026-09-22 16:22:11 -0300

    substituição da planilha problematica

--- Aula_3/Código/Aula_3.py
+++ Aula_3/Código/Aula_3.py
@@ -1,35 +1,34 @@
 import pandas as pd
-import numpy as np
 from pathlib import Path
-import matplotlib.pylab as plt
+import numpy as np
 
 caminho = Path(__file__).resolve()
 raiz = caminho.parent.parent
-planilha = raiz / 'Planilha' / 'Distillation Column Dataset.csv'
+planilha = raiz / 'Planilha' / 'petrochemical_advanced_data.csv'
 
 df = pd.read_csv(planilha)
 
-df['T1 atraso 30 min'] = df['Sensor1'].shift(1)
-df['T1 atraso 60 min'] = df['Sensor1'].shift(2)
-df['T1 atraso 90 min'] = df['Sensor1'].shift(3)
+df['T atraso 30 min'] = df['Reactor_Temp_C'].shift(1)
+df['T atraso 60 min'] = df['Reactor_Temp_C'].shift(2)
+df['T atraso 90 min'] = df['Reactor_Temp_C'].shift(3)
 
-df['T1 atrasado 90 min suave'] = df['T1 atraso 90 min'].rolling(3).mean()
-df['dT1 coluna'] = df['T1 atraso 90 min'].diff()
+df['T atrasado 90 min suave'] = df['T atraso 90 min'].rolling(3).mean()
+df['dT coluna'] = df['T atraso 90 min'].diff()
 
-df['S5 atraso 30 min'] = df['Sensor5'].shift(1)
-df['S5 atraso 60 min'] = df['Sensor5'].shift(2)
-df['S5 atraso 90 min'] = df['Sensor5'].shift(3)
+df['P atraso 30 min'] = df['Reactor_Pressure_Bar'].shift(1)
+df['P atraso 60 min'] = df['Reactor_Pressure_Bar'].shift(2)
+df['P atraso 90 min'] = df['Reactor_Pressure_Bar'].shift(3)
 
-df['S5 atrasado 90 min suave'] = df['S5 atraso 90 min'].rolling(3).mean()
-df['dS5 coluna'] = df['S5 atraso 90 min'].diff()
+df['P atrasado 90 min suave'] = df['P atraso 90 min'].rolling(3).mean()
+df['dP coluna'] = df['P atraso 90 min'].diff()
 
 df.dropna(inplace=True)
 print(f"Shape final: {df.shape}")
-print(f"Features: {[c for c in df.columns if c != 'conversao']}")
-print(f"Target: Fração")
+print(f"Features: {[c for c in df.columns if c != 'Product_Yield_Tons']}")
+print(f"Target: Product_Yield_Tons")
 
-corr = df.corr()['MoleFractionTX'].sort_values(ascending=False)
-print("Correlação de cada feature com a conversão:")
+corr = df.corr()['Product_Yield_Tons'].sort_values()
+print("Correlação de cada feature com a produção:")
 print(corr)
 print(f"\nTop 5 features:")
 print(corr.head(5))
